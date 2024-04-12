a7118b4e3bf3ec033b0bab9fb0c14ac1778d9d8d
mohamedeliwa <mohamed.av11@gmail.com>
2024-04-13 01:19:24 +0200

    extracting command line arguments,  parsing config file, extracting the actual command from the config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use config::{Config, ConfigError, File, FileFormat};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// the alias for the command/s to execute
+    alias: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct MyConfig {
+    /// a list of all aliases sourced from the config file
+    aliases: Value,
+}
+
+fn main() -> Result<(), ConfigError> {
+    // extracting the command alias from the cli arguments
+    let args: Cli = Cli::parse();
+
+    // getting the list of aliases from the config file
+    let settings = Config::builder()
+        .set_default("aliases", "{}")?
+        .add_source(File::new("rupit", FileFormat::Json))
+        .build()
+        .unwrap();
+
+    let settings: MyConfig = settings.try_deserialize::<MyConfig>().unwrap();
+
+    // getting the actual command from the config file
+    let alias = args.alias;
+    let command = settings.aliases[&alias].clone();
+
+    println!("alias: {:?}", alias);
+    println!("command: {:?}", command);
+
+    // executing the command
+    todo!();
+
+    Ok(())
 }
