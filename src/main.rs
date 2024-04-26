50ba2ecee71f67976338215ecde88212e04e1597
mohamedeliwa <mohamed.av11@gmail.com>
2024-04-26 19:01:34 +0300

    allowing sub commands and args groups

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,59 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use config::ConfigError;
 
 mod runner;
 mod settings;
 
 #[derive(Parser, Debug)]
+#[command(version, propagate_version = true)]
 struct Arguments {
-    /// the alias for the command/s to execute
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Runs the corresponding command to the specified alias
+    Run(RunArgs),
+    /// Shows config data based on the args specified
+    Show(ShowArgs),
+}
+
+#[derive(Args, Debug)]
+struct RunArgs {
+    /// the alias for the command to execute
     alias: String,
+}
+
+#[derive(Args, Debug)]
+#[group(required = true, multiple = false)]
+struct ShowArgs {
+    /// Prints the path of the config file
+    #[arg(long, short)]
+    config: bool,
+    /// Prints the list of the available aliases in the config file
+    #[arg(long, short)]
+    aliases: bool,
 }
 
 fn main() -> Result<(), ConfigError> {
     // extracting the command alias from the cli arguments
     let args: Arguments = Arguments::parse();
-    let alias = &args.alias;
-    println!("\nalias: {:?}", alias);
 
-    // getting the actual command from the config file
-    let command = settings::get_command_for_alias(alias).unwrap();
+    match args.command {
+        Commands::Run(args) => {
+            let alias = &args.alias;
+            println!("\nalias: {:?}", alias);
 
-    // executing the command
-    runner::run_command(&command);
+            // getting the actual command from the config file
+            let command = settings::get_command_for_alias(alias).unwrap();
 
+            // executing the command
+            runner::run_command(&command);
+        }
+        Commands::Show(args) => {
+            println!("{:?}", args)
+        }
+    }
     Ok(())
 }
