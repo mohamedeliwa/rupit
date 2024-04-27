5a5a822fd1ce5d357658539350dff57e7176a0f6
mohamedeliwa <mohamed.av11@gmail.com>
2024-04-27 18:32:23 +0300

    better error handling for root module

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,6 @@ struct ShowArgs {
 }
 
 fn main() -> Result<(), ConfigError> {
-    // extracting the command alias from the cli arguments
     let args: Arguments = Arguments::parse();
 
     match args.command {
@@ -45,15 +44,16 @@ fn main() -> Result<(), ConfigError> {
             let alias = &args.alias;
             println!("\nalias: {:?}", alias);
 
-            // getting the actual command from the config file
-            let command = settings::get_command_for_alias(alias).unwrap();
+            let command = settings::get_command_for_alias(alias)?;
 
-            // executing the command
             runner::run_command(&command);
         }
         Commands::Show(args) => {
             if args.config {
-                let config_file_path = settings::get_config_file_path();
+                let config_file_path = settings::get_config_file_path().ok_or(
+                    ConfigError::Message(String::from("couldn't get config file path from the OS")),
+                )?;
+
                 println!("\n Rupit's config file path is:");
                 println!("\n {:?}", config_file_path);
             } else if args.aliases {
